b865307a68e11f0b95353f6f7014f185463f27fe
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-02-02 20:57:08 +0500

    added option to add new student and view all students

--- main.cpp
+++ main.cpp
@@ -1,21 +1,50 @@
 #include <iostream>
 #include <stdlib.h>
+#include <windows.h>
+#include "student.h"
 using namespace std;
 
 int mainMenu();
 
 int main(){
     int choice;
-    while(1){
+    Student s;
 
+    while(1){
+        choice = mainMenu();
+        switch(choice){
+            case 1:
+                s.setData();
+                break;
+            case 2:
+                s.showAllStudents();
+                break;
+            // case 3:
+            //     s.searchStudent();
+            //     break;
+            // case 4:
+            //     s.deleteStudent();
+            //     break;
+            // case 5:
+            //     s.modifyStudent();
+            //     break;
+            default:
+                cout << "Wrong choice entered, please enter a valid choice" << endl;
+                Sleep(3000);
+        }
     }
     return 0;
 }
 
 int mainMenu(){
+    int choice;
     system("cls");
     cout << "1. Add Student" << endl;
     cout << "2. View All Students" << endl;
-    cout << "3. Delete Student" << endl;
-    cout << "4. Modify Student Details" << endl;
+    cout << "3. Search Student" << endl;
+    cout << "4. Delete Student" << endl;
+    cout << "5. Modify Student Details" << endl << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    return choice;
 }
--- student.h
+++ student.h
@@ -1,24 +1,115 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <fstream>
 #include <conio.h>
 #include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 class Student{
     private:
         int rollNo;
-        string name;
+        char name[20];
         int age;
-        string email;
-        string contactNumber;
+        char email[20];
+        char contactNumber[20];
 
         int generateRollNo();
+        void storeData();
     public:
-        Student();
         void setData();
+        void showData();
+        void showAllStudents();
+        // void searchStudent();
+        // void deleteStudent();
+        // void modifyStudent();
 };
+
+int Student::generateRollNo(){
+    ifstream fin;
+    int id;
+    bool isFound = false;
+    srand(time(0));
+    fin.open("student.dat", ios::in|ios::binary);
+    if(!fin){
+        cout << "File does not exists" << endl;
+    }else{
+        while(1){
+            id = (rand() % 10000) + 100000;
+            fin.read((char*)this, sizeof(*this));
+            while(fin.eof() == 0){
+                if(this->rollNo == id){
+                    isFound = true;
+                    break;
+                }
+                fin.read((char*)this, sizeof(*this));
+            }
+            if(isFound == false){
+                break;
+            }
+        }
+    }
+    fin.close();
+    return id;
+}
+
+void Student::setData(){
+    this->rollNo = generateRollNo();
+    system("cls");
+    cout << "Enter name: ";
+    fflush(stdin);
+    gets(name);
+    cout << "Enter age: ";
+    fflush(stdin);
+    cin >> this->age;
+    cout << "Enter email: ";
+    fflush(stdin);
+    gets(email);
+    cout << "Enter contact number: ";
+    fflush(stdin);
+    gets(contactNumber);
+    
+    storeData();
+}
+
+void Student::showData(){
+    system("cls");
+    cout << "ID: " << this->rollNo << endl;
+    cout << "Name: " << this->name << endl;
+    cout << "Age: " << this->age << endl;
+    cout << "Email: " << this->email << endl;
+    cout << "Contact Number: " << this->contactNumber << endl << endl;
+    cout << "Press any key to continue...";
+    getch();
+}
+
+void Student::storeData(){
+    ofstream fout;
+    fout.open("student.dat", ios::app|ios::binary);
+    fout.write((char*)this, sizeof(*this));
+    fout.close();
+}
+
+void Student::showAllStudents(){
+    ifstream fin;
+    fin.open("student.dat", ios::in|ios::binary);
+    if(!fin){
+        cout << "File does not exists" << endl;
+    }else{
+        fin.read((char*)this, sizeof(*this));
+        while(fin.eof() == 0){
+            cout << "ID: " << this->rollNo << endl;
+            cout << "Name: " << this->name << endl;
+            cout << "Age: " << this->age << endl;
+            cout << "Email: " << this->email << endl;
+            cout << "Contact Number: " << this->contactNumber << endl << endl;
+            fin.read((char*)this, sizeof(*this));
+        }
+    }
+    fin.close();
+    getch();
+}
 
 #endif
